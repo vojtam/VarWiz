d4617dc74cf8a226f07e2bcd47cf418a7547e2c3
vojtam <macala.vojtech@gmail.com>
2023-12-10 14:50:54 +0100

    link selection function reorganization

--- application/app/view/sankey.R
+++ application/app/view/sankey.R
@@ -37,10 +37,25 @@ server <- function(id, dataset, selected_pathways_react) {
     # reactive value holding the sankey plot
     sankey_plot <- reactiveVal()
     labels <- reactiveVal()
-    sankey_data <- reactiveValues()
     path_gene_tab <- reactiveVal()
     gene_variant_tab <- reactiveVal()
     
+
+# Render ------------------------------------------------------------------
+
+    output$sankey_plt <- renderPlotly({
+      validate(
+        need(!is.null(selected_pathways_react()), "select the pathways that you want to visualize from the table on the right")
+      )
+      # output the contents of the sankey_plot reactive value
+      sankey_plot()
+    })
+    
+    
+    
+
+# Events ------------------------------------------------------------------
+
     # data is passed into module -> create sankey
     observeEvent(selected_pathways_react(), {
       req(selected_pathways_react())
@@ -57,54 +72,14 @@ server <- function(id, dataset, selected_pathways_react) {
     })
     
     
-    output$sankey_plt <- renderPlotly({
-      validate(
-        need(!is.null(selected_pathways_react()), "select the pathways that you want to visualize from the table on the right")
-      )
-      # output the contents of the sankey_plot reactive value
-      sankey_plot()
-    })
-    # 
-    # observeEvent(event_data("plotly_hover", priority = "event"), {
-    #   data <- event_data("plotly_hover", priority = "event")
-    #   req(data)
-    #   browser()
-    # })
-    
     observeEvent(event_data("plotly_click", priority = "event"), {
       click_data <- event_data("plotly_click", priority = "event")
       req(click_data)
       
+      link_ids <- sankey_link_selection(click_data, path_gene_tab(), gene_variant_tab())
       
-      clicked_link <- click_data$pointNumber + 1
-      if (clicked_link <= nrow(path_gene_tab())) {
-        select_gene_name <- path_gene_tab()[clicked_link]$gene_name
-        first_links <- clicked_link + 1
-        second_links <- gene_variant_tab()[gene_name == select_gene_name]$third
-        if (nrow(unique(path_gene_tab()[, .(kegg_paths_name)])) > 1) {
-          second_links <- second_links - 1
-        }
-        link_ids <- unique(c(
-          first_links,
-          second_links)
-        )
-      } else {
-        select_gene_name <- gene_variant_tab()[(clicked_link - nrow(path_gene_tab()))]$gene_name
-        first_links <- which(path_gene_tab()$gene_name == select_gene_name) + 1
-        second_links <- gene_variant_tab()[gene_name == select_gene_name]$third
-        if (nrow(unique(path_gene_tab()[, .(kegg_paths_name)])) > 1) {
-          second_links <- second_links - 1
-        }
-        #second_links <- second_links[1: length(second_links) - 1]
-        link_ids <- unique(c(
-          first_links,
-          second_links)
-        )
-      }
-      #browser()
-
-      num_links <- nrow(labels())
-      link_colors <- rep("lightgray", num_links)
+    
+      link_colors <- rep("lightgray", nrow(labels()))
       link_colors[link_ids - 1] <- "red"
 
       proxy <- plotlyProxy("sankey_plt", session)
@@ -134,12 +109,77 @@ server <- function(id, dataset, selected_pathways_react) {
       )
       shinyjs::runjs(js_call)
     }, ignoreInit = TRUE)
-  
-     
   })
 }
 
 
+#' sankey_link_selection
+#'
+#' @param click_data 
+#' @param path_gene_tab 
+#' @param gene_variant_tab 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+sankey_link_selection <- function(click_data, path_gene_tab, gene_variant_tab) {
+  clicked_link <- click_data$pointNumber + 1
+  
+  if (clicked_link <= nrow(path_gene_tab)) {
+    return(first_level_link_selection(clicked_link, path_gene_tab, gene_variant_tab))
+  } 
+  return(second_level_link_selection(clicked_link, path_gene_tab, gene_variant_tab))
+}
 
+
+#' first_level_link_selection
+#'
+#' @param clicked_link 
+#' @param path_gene_tab 
+#' @param gene_variant_tab 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+first_level_link_selection <- function(clicked_link, path_gene_tab, gene_variant_tab) {
+  select_gene_name <- path_gene_tab[clicked_link]$gene_name
+  first_links <- clicked_link + 1
+  second_links <- gene_variant_tab[gene_name == select_gene_name]$third
+  if (nrow(unique(path_gene_tab[, .(kegg_paths_name)])) > 1) {
+    second_links <- second_links - 1
+  }
+  link_ids <- unique(c(
+    first_links,
+    second_links)
+  )
+  return(link_ids)
+}
+
+
+#' second_level_link_selection
+#'
+#' @param clicked_link 
+#' @param path_gene_tab 
+#' @param gene_variant_tab 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+second_level_link_selection <- function(clicked_link, path_gene_tab, gene_variant_tab) {
+  select_gene_name <- gene_variant_tab[(clicked_link - nrow(path_gene_tab))]$gene_name
+  first_links <- which(path_gene_tab$gene_name == select_gene_name) + 1
+  second_links <- gene_variant_tab[gene_name == select_gene_name]$third
+  if (nrow(unique(path_gene_tab[, .(kegg_paths_name)])) > 1) {
+    second_links <- second_links - 1
+  }
+  link_ids <- unique(c(
+    first_links,
+    second_links)
+  )
+  return(link_ids)
+}
 
 
